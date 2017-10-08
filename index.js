a8bcd1c373f39b65b796262505054de949452446
Kashyap <kashyap.kmbc@gmail.com>
2017-10-08 21:32:14 +0530

    Basic implementation

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
+const get = require('lodash.get');
+const t = require('babel-types');
+
+let state = 0;
+
+// Counts the number of arguments to console.log that are variables.
+const countLabelArgumentsVisitor = {
+	Identifier: function (path, args) {
+		const name = path.node.name;
+
+		if (name !== 'console' && name !== 'log') {
+			args.items.push(path.node.name);
+		}
+	}
+};
+
 const transformer = function (babel) {
 	return {
 		visitor: {
-			Identifier: function (path, state) {
-				if (path.node.name === 'log') {
-					if (path.parent.object.name === 'console') {
-						const args = path.parentPath.parentPath.node.arguments;
-						console.log(args);
+			CallExpression: (path) => {
+				const obj = get(path, 'node.callee.object.name');
+				const prop = get(path, 'node.callee.property.name')
+
+				if (obj === 'console' && prop === 'log') {
+					const argCount = path.node.arguments.length;
+					const items = [];
+					path.traverse(countLabelArgumentsVisitor, { items })
+					if (items.length === argCount) {
+						const names = path.node.arguments.map(node => node.name);
+						const concated = `${names.join(', ')}: `;
+						path.node.arguments.unshift(t.stringLiteral(concated));
 					}
 				}
 			}
